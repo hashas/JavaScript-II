aa87b77453d14df79be811341031d974177c2007
hh <hh@hhmbp15.local>
2020-01-14 02:16:00 +0000

    completed all assignment, partially completed stretch

--- assignments/closure.js
+++ assignments/closure.js
@@ -4,19 +4,43 @@
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
 
+const a = 7
+function multiplyByTwo(x) {
+  let ret = x * 2;
+  return ret;
+}
+
+let b = multiplyByTwo(a);
+console.log(b)
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Implement a "counter maker" function ====
+// let count = 0
 const counterMaker = () => {
   // IMPLEMENTATION OF counterMaker:
   // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
+    let count = 0
   // 2- Declare a function `counter`. It should increment and return `count`.
   //      NOTE: This `counter` function, being nested inside `counterMaker`,
   //      "closes over" the `count` variable. It can "see" it in the parent scope!
+
+    function counter() {
+      count++
+      return count
+    }
+  
   // 3- Return the `counter` function.
+    return counter
+
 };
+
+const myCounter = counterMaker();
+console.log(myCounter())
+console.log(myCounter())
+console.log(myCounter())
+
 // Example usage: const myCounter = counterMaker();
 // myCounter(); // 1
 // myCounter(); // 2
